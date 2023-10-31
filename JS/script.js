1ae7e65328bd29d5db4d124aced445f32412f958
CrnaStrela94 <luka940114@gmail.com>
2023-10-31 13:06:05 +0100

    luka

--- JS/script.js
+++ JS/script.js
@@ -1,63 +1,95 @@
-/************************ calling on all classes in a DOM *******************************/
-// const sun = document.querySelector(`sun`);
-// const mercury = document.querySelector(`mercury`);
-// const venus = document.querySelector(`venus`);
-// const earth = document.querySelector(`earth`);
-// const mars = document.querySelector(`mars`);
-// const jupiter = document.querySelector(`jupiter`);
-// const saturn = document.querySelector(`saturn`);
-// const uranus = document.querySelector(`uranus`);
-// const neptune = document.querySelector(`neptune`);
-/****************************************************************************************/
+document.addEventListener("DOMContentLoaded", main);
 
-document.addEventListener("DOMContentLoaded", () => {
+async function main() {
+  const apiKey = await getApiKey();
+  const bodies = await getBodies(apiKey);
+  console.log(bodies);
+
   document.querySelectorAll(".planet").forEach((planet) => {
     planet.addEventListener("click", function () {
       const planetName = this.getAttribute("data-name");
-      fetchPlanetData(planetName);
+      fetchPlanetData(planetName, apiKey);
     });
   });
-});
-
-function fetchPlanetData(planetName) {
+}
+// let resp = await fetch(
+//   "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+//   {
+//     method: "GET",
+//     headers: { "x-zocom": "${apiKey}" },
+//   }
+// );
+async function fetchPlanetData(planetName, apiKey) {
   const planetElement = document.querySelector(`[data-name="${planetName}"]`);
   let infoElement = planetElement.querySelector(".planet-info");
 
-  if (infoElement) {
+  if (!infoElement) {
+    infoElement = document.createElement("div");
+    infoElement.classList.add("planet-info");
+    planetElement.appendChild(infoElement);
+  } else {
     infoElement.remove();
     return;
   }
 
-  infoElement = document.createElement("div");
-  infoElement.classList.add("planet-info");
-  planetElement.appendChild(infoElement);
-
-  $.ajax({
-    method: "GET",
-    url: "https://api.api-ninjas.com/v1/planets?name=" + planetName,
-    headers: { "X-Api-Key": "mnQDUd+rGtrmVnUFL89gjQ==soE534Ga6GGpGzv3" },
-    contentType: "application/json",
-    success: function (result) {
-      if (result.length > 0) {
-        const planetData = result[0];
-        displayPlanetInfo(planetData, infoElement);
-      } else {
-        infoElement.innerHTML = "No information available";
-      }
-    },
-    error: function (jqXHR) {
-      console.error("Error: ", jqXHR.responseText);
-      infoElement.innerHTML = "Failed to load information";
-    },
-  });
+  try {
+    const bodies = await getBodies(apiKey);
+    const planetData = bodies.find((planet) => planet.name === planetName);
+    if (planetData) {
+      infoElement.textContent = `Details for ${planetName}: ${JSON.stringify(
+        planetData
+      )}`;
+    } else {
+      infoElement.textContent = `No data found for ${planetName}`;
+    }
+  } catch (error) {
+    console.error(`Error fetching planet data:`, error);
+    infoElement.textContent = `Error fetching planet data for ${planetName}`;
+  }
 }
 
-function displayPlanetInfo(planetData, infoElement) {
-  infoElement.innerHTML = `
-    <p>Name: ${planetData.name}</p>
-    <p>Mass: ${planetData.mass}</p>
-    <p>Radius: ${planetData.radius}</p>
-    <p>Period: ${planetData.period}</p>
-    <p>Semi Major Axis: ${planetData.semi_major_axis}</p>
-  `;
+const url = "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com";
+
+async function getApiKey() {
+  try {
+    const response = await fetch(
+      `https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/keys`,
+      {
+        method: "POST",
+      }
+    );
+
+    const { body, ok, ...rest } = response;
+
+    console.log(JSON.stringify(body), ok);
+    return body.key;
+  } catch (error) {
+    console.error("Error fetching API key:", error);
+  }
+}
+
+async function getBodies(apiKey) {
+  console.log("API key;", apiKey);
+  try {
+    const response = await fetch(
+      "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies",
+      {
+        method: "GET",
+        mode: "no-cors",
+        headers: { "x-zocom": apiKey },
+      }
+    );
+
+    console.log(response);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    return data.bodies || [];
+  } catch (error) {
+    console.error("Error fetching bodies:", error);
+    return [];
+  }
 }
